9b8e7ebd806cdb0115ec6e7250a3df5a853f5824
Ronando <ronando.long@gmail.com>
2016-01-14 07:04:53 -0800

    It will count from 99 to 0 and list the lyrics to the song.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  var numberBottles = (99..0);
+  // var numberBottles = (99..0);
   var lyrics1 = (" bottles of beer on the wall, ");
-  var lyrics2 = (" bottles of beer. Take one down, pass it around,");
+  var lyrics2 = (" bottles of beer. Take one down, pass it around, ");
   var lyrics3 = (" bottles of beer on the wall.");
-  var count = 99;
 
-  $("button .ninety").click(function() {
-    for(var i = 0; i >= 0; i -= 1) {
-      count -= i;
-      $("ul.list").append("<li>" + count + lyrics1 + count + lyrics2 + count + lyrics3 + "</li>")
+  $("button.ninety").click(function() {
+    for(var i = 99; i >= 0; i -= 1) {
+      // count -= i;
+      $("ul.list").append("<li>" + i + lyrics1 + i + lyrics2 + (i-1) + lyrics3 + "</li>")
     }
 
     $(".results").show();
